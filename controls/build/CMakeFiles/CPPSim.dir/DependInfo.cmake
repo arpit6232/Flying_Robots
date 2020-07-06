671ab8b06098703227bb5de9078525900026368b
arpit <arpit.savarkar10@gmail.com>
2020-07-05 18:07:10 -0600

    Controls of Flying Drones

---
+++ controls/build/CMakeFiles/CPPSim.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/BaseController.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/BaseController.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/ColorUtils.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/ColorUtils.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/DrawingFuncs.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/DrawingFuncs.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/GLUTMenu.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/GLUTMenu.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/Graph.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/Graph.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/GraphManager.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/GraphManager.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Drawing/Visualizer_GLUT.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Drawing/Visualizer_GLUT.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Math/Geometry.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Math/Geometry.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Math/Random.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Math/Random.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/MavlinkNode/MavlinkNode.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/MavlinkNode/MavlinkNode.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/MavlinkNode/MavlinkTranslation.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/MavlinkNode/MavlinkTranslation.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/MavlinkNode/PracticalSocket.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/MavlinkNode/PracticalSocket.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/QuadControl.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/QuadControl.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/BaseDynamics.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/BaseDynamics.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/QuadDynamics.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/QuadDynamics.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/Simulator.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/Simulator.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/magnetometer.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/magnetometer.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/opticalflow.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/opticalflow.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Simulation/rangefinder.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Simulation/rangefinder.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Trajectory.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Trajectory.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Utility/Camera.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Utility/Camera.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Utility/SimpleConfig.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Utility/SimpleConfig.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/Utility/Timer.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/Utility/Timer.cpp.o"
+  "/home/arpit/arpit/flying/Flying_Robots/controls/src/main.cpp" "/home/arpit/arpit/flying/Flying_Robots/controls/build/CMakeFiles/CPPSim.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../lib"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
